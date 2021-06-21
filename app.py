71198902d39dc8a2d50ebb01adb063c8fd3f492a
Marius Weidner <weidner@cs.uni-freiburg.de>
2021-06-21 17:19:01 +0200

    oppsie

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ app = create_app({
     # OMG!!!! he posted the secret Key!1133!!!
     'SECRET_KEY': 'V6B3cCsuaMbbffrk',
     # local database storage
-    'SQLALCHEMY_DATABASE_URI': 'sqlite:///store.db'
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:////data/store.db'
 })
-
